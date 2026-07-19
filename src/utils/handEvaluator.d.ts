635523479354b05ad5fce5024e16bf8d3c4d7066
s4lmon778 <cheuklam7788@gmail.com>
2026-07-18 21:47:41 -0700

    chore: clean up leaked build artifacts from src/ and add .gitignore

--- src/utils/handEvaluator.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Card, HandEvaluation } from '../types/card';
-export declare function evaluateHand(holeCards: Card[], communityCards: Card[]): HandEvaluation;
-//# sourceMappingURL=handEvaluator.d.ts.map
